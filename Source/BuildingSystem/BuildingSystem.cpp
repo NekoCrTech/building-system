2fe2985a21ef1e42cf73afb7964717d99f31c9c2
nektpol <nekocreativetechnologies@gmail.com>
2025-07-10 13:05:53 +0300

    Initialize Project

---
+++ Source/BuildingSystem/BuildingSystem.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BuildingSystem.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BuildingSystem, "BuildingSystem" );
+
+DEFINE_LOG_CATEGORY(LogBuildingSystem)
+ 
---
+++ Source/BuildingSystem/BuildingSystemGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BuildingSystemGameMode.h"
+
+ABuildingSystemGameMode::ABuildingSystemGameMode()
+{
+	// stub
+}
